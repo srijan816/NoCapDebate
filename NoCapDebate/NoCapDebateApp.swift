63b760eb2738ab33fcfcacb0d241ff0c8a2e4080
Srijan Poudel <srijanpoudel@Srijans-Mac-mini.local>
2026-03-06 03:31:14 +0800

    Restore NoCapDebate project files

---
+++ NoCapDebate/NoCapDebateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoCapDebateApp.swift
+//  NoCapDebate
+//
+//  Created by Srijan Poudel on 5/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct NoCapDebateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
